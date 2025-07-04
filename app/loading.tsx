19d821cf06e174a6fc2330f9fca19812fd2d7639
Simeon Tsvetanov <tsvetanov.simeon@gmail.com>
2025-07-04 22:52:35 +0300

    deploy: static export

--- app/loading.tsx
+++
@@ -1,9 +0,0 @@
-import { LoadingScreen } from "@/components/ui/loading-screen"
-
-/**
- * Global loading component for Next.js
- * Shows while pages are loading
- */
-export default function Loading() {
-  return <LoadingScreen />
-}
